18bbf44941b7a126e9f5be79cd645e0609dd93f6
madsfjeldberg <mafj0001@stud.kea.dk>
2025-05-04 21:43:22 +0200

    initial scraping test

--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -1,11 +1,32 @@
 <script>
   import { onMount } from "svelte";
   import { goto } from "$app/navigation";
+  import Button from "$lib/components/ui/button/button.svelte";
 
   export let data;
   const { user } = data;
 
   let username = user.username;
+
+  const scrapeSite = async () => {
+    const response = await fetch("http://localhost:9000/api/scrape", { // Added http://
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ url: "https://www.instagram.com/p/C3TIKgKvAXo/" }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to scrape the site");
+    }
+
+    const result = await response.json();
+    console.log(result); // Log the result to the console
+    return result;
+
+  }
 
 </script>
 
@@ -16,3 +37,4 @@
 
 <h1 class="font-bold text-3xl text-center dark:text-gray-200">Welcome to the Dashboard, {username}!</h1>
 <h2 class="font-bold text-xl text-center dark:text-gray-200">This means you're logged in :)</h2>
+<Button onclick={scrapeSite}>Scrape insta link</Button>
