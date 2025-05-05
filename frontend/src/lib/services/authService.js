f89cdf1098660bbc552ff4911df476378ce5b80c
Chris <chye0001@stud.kea.dk>
2025-05-05 21:57:57 +0200

    added 2 functions, reseting- and sendRestPasswordRequest and exported these in authService

--- frontend/src/lib/services/authService.js
+++ frontend/src/lib/services/authService.js
@@ -1,4 +1,5 @@
 import { isAuthenticated } from "$lib/stores/authStore";
+import { makeOption } from "./util.js";
 
 const BASE_URL = import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/auth` : '/api/auth';
 // const BASE_URL = '/api/auth';
@@ -51,8 +52,38 @@ const logout = async () => {
   return true;
 }
 
+
+
+async function sendRestPasswordRequest(email) {
+  try{
+    const postOption = makeOption("POST", {email})
+    const response = await fetch(`${BASE_URL}/forgot-password`, postOption);
+    return await response.json();
+
+  } catch(error) {
+    console.error(error);
+  }  
+}
+
+
+
+async function resetPassword(newPassword, resetToken) {
+  try{
+    const patchOption = makeOption("PATCH", {newPassword});
+    const response = await fetch(`${BASE_URL}/reset-password/${resetToken}`, patchOption);
+    return await response.json();
+
+  }catch(error) {
+    console.error(error);
+  }
+}
+
+
+
 export const authService = {
   login,
   register,
-  logout
+  logout,
+  sendRestPasswordRequest,
+  resetPassword
 };
