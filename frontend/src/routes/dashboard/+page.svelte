2a8678e7aa51e23e2087bc8a31a6ce42a0ec7fb2
Chris <chye0001@stud.kea.dk>
2025-05-06 21:20:25 +0200

    added selectedRecipe as bindable to add-list-dialog.svelte to ensure new recipelist creation would change view to the newly created list. Also added spacing making code more readable

--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -7,19 +7,6 @@
 
   let username = user.username;
 
-  async function test() {
-    console.log("asfda")
-
-    const response = await fetch("https://api.calorieninjas.com/v1/nutrition?query="+"200g chicken, 51g brocoli", {
-      headers: {
-        "X-Api-Key": "C3Fd5ykDV1g1T5sRhK6Nxg==UHOtfGzcE89aZYxU"
-      }
-    });
-
-    const result = await response.json();
-    console.log(result);
-    
-  }
 </script>
 
 <svelte:head>
@@ -29,5 +16,3 @@
 
 <h1 class="font-bold text-3xl text-center dark:text-gray-200">Welcome to the Dashboard, {username}!</h1>
 <h2 class="font-bold text-xl text-center dark:text-gray-200">This means you're logged in :)</h2>
-
-<button onclick={test}>Test</button>
