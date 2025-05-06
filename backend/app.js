af16a7bc6515a80291a041a8697ba932f47b5b9e
Chris <chye0001@stud.kea.dk>
2025-05-06 15:06:33 +0200

    created router for macros acts as a reverse proxy, to hide api keys from frontend code

--- backend/app.js
+++ backend/app.js
@@ -38,4 +38,7 @@ app.use(recipesRouter);
 app.use(usersRouter);
 app.use(categoriesRouter);
 
+import macroRouter from './routers/macroRouter.js';
+app.use(macroRouter);
+
 app.listen(PORT, () => {console.log(`Server is running on port ${PORT}.`)});
