c8d6b82cd467e78a9ad2ab0444897b518eaaf7c9
Chris <chye0001@stud.kea.dk>
2025-05-08 20:54:48 +0200

    created tagsApi.js in frontend for fetching all tags in db

---
+++ frontend/src/lib/api/tagsApi.js
@@ -0,0 +1,24 @@
+import { makeOption } from "./util";
+
+const BASE_URL = import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/tags` : '/api/tags';
+
+const getRecipeTags = async () => {
+    try {
+        const getOption = makeOption("GET");
+        const response = await fetch(BASE_URL, getOption);
+
+        if(!response.ok) {
+            const error = await response.json();
+            return error.errorMessage;
+        }
+
+        const result = await response.json();
+        return result.data;
+
+    }catch(error) {
+        console.error(error);
+        throw new Error(error.message);
+    }
+}
+
+export default {getRecipeTags}
