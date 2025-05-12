06ca159c1653348d0ea3791ad6665d32bb838292
Chris <chye0001@stud.kea.dk>
2025-05-12 21:23:45 +0200

    created new repo function for posting comment reply, and implimented new socket for handling comments reply

--- backend/repository/commentsRepository.js
+++ backend/repository/commentsRepository.js
@@ -1,7 +1,32 @@
 import prisma from "../database/prismaClient.js"
 
-const postComment = async (userId, text, recipeId) => {
+
+const getCommentsByRecipeId = async (recipeId) => {
     try{
+
+        const comments = await prisma.comment.findMany({
+            where: { recipeId, parentId: null },
+            include: {
+                replies: {
+                    include: {
+                        user: { select: { username: true } }
+                    }
+                },
+                user: { select: { username: true } }
+            }
+        });
+        return comments;
+
+    }catch (error) {
+        console.error(error);
+        throw new Error("Could not fetch all comments for the recipe with id:", recipeId);
+    }
+}
+
+
+
+const postComment = async (userId, text, recipeId) => {
+    try {
         const postedComment = await prisma.comment.create({
             data: {
                 comment: text,
@@ -26,7 +51,7 @@ const postComment = async (userId, text, recipeId) => {
         })
         return postedComment;
 
-    }catch(error) {
+    } catch (error) {
         console.error(error);
         throw new Error("Could not post comment");
     }
@@ -34,4 +59,39 @@ const postComment = async (userId, text, recipeId) => {
 
 
 
-export default { postComment }
+const postCommentReply = async (userId, text, recipeId, commentParentId) => {
+    try {
+        const postedComment = await prisma.comment.create({
+            data: {
+                parentId: commentParentId,
+                comment: text,
+                user: {
+                    connect: {
+                        id: userId
+                    }
+                },
+                recipe: {
+                    connect: {
+                        id: recipeId
+                    }
+                }
+            },
+            include: {
+                user: {
+                    select: {
+                        username: true
+                    }
+                }
+            }
+        })
+        return postedComment;
+
+    } catch (error) {
+        console.error(error);
+        throw new Error("Could not post comment");
+    }
+};
+
+
+
+export default { getCommentsByRecipeId, postComment, postCommentReply }
--- backend/sockets/socketHandlers/commentsHandler.js
+++ backend/sockets/socketHandlers/commentsHandler.js
@@ -5,9 +5,19 @@ export const commentsHandler = (socket, io) => {
         console.log("comment recived:", newComment);
 
         const postedComment = await commentsRepository.postComment(newComment.userId, newComment.comment, newComment.recipeId);
-
         socket.broadcast.emit("new-comment", postedComment);
         
         console.log("comment emitted:", postedComment)
+    })
+
+
+
+    socket.on("new-comment-reply", async (newReply) => {
+        console.log("reply recived:", newReply);
+
+        const postedCommentReply = await commentsRepository.postCommentReply(newReply.userId, newReply.comment, newReply.recipeId, newReply.commentParentId);
+        socket.broadcast.emit("new-comment-reply", postedCommentReply);
+        
+        console.log("reply emitted:", postedCommentReply);
     })
 }
