8b20cb01f3ef478d834e531e16c56a49a9961eae
Chris <chye0001@stud.kea.dk>
2025-05-13 21:33:49 +0200

    created recipeRepository containing getRecipeById used in commentsHandler, to attach recipeName on a comment, so its included in email notification

--- backend/sockets/socketHandlers/commentsHandler.js
+++ backend/sockets/socketHandlers/commentsHandler.js
@@ -1,12 +1,17 @@
 import emailService from "../../service/emailService.js";
 import commentsRepository from "../../repository/commentsRepository.js";
+import recipeRepository from "../../repository/recipeRepository.js";
 
 export const commentsHandler = (socket, io) => {
     socket.on("new-comment", async (newComment) => {
         console.log("comment recived:", newComment);
 
         const postedComment = await commentsRepository.postComment(newComment.userId, newComment.comment, newComment.recipeId);
-        //TOdo notifi recipe creator that someone has commented on their recipe.
+        
+        const recipeCreatorUsername = postedComment.recipe.recipeLists[0].user.username;
+        const recipeCreatorEmail = postedComment.recipe.recipeLists[0].user.email;
+        emailService.sendCommentNotification(recipeCreatorEmail, recipeCreatorUsername, postedComment);
+
         io.emit("new-comment", postedComment);
         
         console.log("comment emitted:", postedComment)
@@ -18,7 +23,18 @@ export const commentsHandler = (socket, io) => {
         console.log("reply recived:", newReply);
 
         const postedCommentReply = await commentsRepository.postCommentReply(newReply.userId, newReply.comment, newReply.recipeId, newReply.commentParentId);
-        //todo enable email notifications
+        
+        if(!newReply.replyParent) {
+            const parentComment = await commentsRepository.getCommentById(newReply.commentParentId);
+            emailService.sendCommentReplyNotification(parentComment, postedCommentReply);
+            
+        } else {
+            const foundRecipe = await recipeRepository.getRecipeById(newReply.recipeId);
+            const recipeName = foundRecipe.name;
+            newReply.replyParent.recipe = { name: recipeName};
+            emailService.sendCommentReplyNotification(newReply.replyParent, postedCommentReply);
+        }
+
         io.emit("new-comment-reply", postedCommentReply);
         
         console.log("reply emitted:", postedCommentReply);
