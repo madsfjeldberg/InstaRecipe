a8bd19170acb58ed1931b6531ed49f525c8a3e08
madsfjeldberg <mafj0001@stud.kea.dk>
2025-05-15 10:13:42 +0200

    add seed.js

---
+++ backend/prisma/seed.js
@@ -0,0 +1,59 @@
+import prisma from '../database/prismaClient.js';
+
+async function main() {
+  
+  const categories = [
+    'Breakfast',
+    'Lunch',
+    'Dinner',
+    'Snack',
+    'Dessert',
+    'Beverage'
+  ];
+
+  const tags = [
+    'Vegan',
+    'Vegetarian',
+    'Gluten-Free',
+    'Dairy-Free',
+    'High-Protein',
+    'Italian',
+    'Chinese',
+    'Mexican',
+    'Indian',
+    'Japanese',
+    'Mediterranean',
+    'American',
+    'French',
+    'Spanish',
+    'Thai',
+  ];
+
+  for (const name of tags) {
+    await prisma.tag.upsert({
+      where: { name },
+      update: {},
+      create: { name }
+    });
+  };
+  console.log('Tags seeded!');
+
+  for (const name of categories) {
+    await prisma.category.upsert({
+      where: { name },
+      update: {},
+      create: { name }
+    });
+  };
+  console.log('Categories seeded!');
+
+}
+
+main()
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
