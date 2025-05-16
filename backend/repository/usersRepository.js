5569369b820b7145d33ddb4684c4757f28130547
Chris <chye0001@stud.kea.dk>
2025-05-16 22:56:01 +0200

    created repo function for following and unfollowing user, also made getting user by id include followers and following.

--- backend/repository/usersRepository.js
+++ backend/repository/usersRepository.js
@@ -3,15 +3,39 @@ import prisma from "../database/prismaClient.js";
 
 
 const getUserById = async (userId) => {
-    try{
+    try {
         const foundUser = await prisma.user.findUnique({
             where: {
                 id: userId
+            },
+            include: {
+                followers: {
+                    select: {
+                        id: true,
+                        username: true,
+                        avatar: true,
+                        avatarMime: true
+                    }
+                },
+                following: {
+                    select: {
+                        id: true,
+                        username: true,
+                        avatar: true,
+                        avatarMime: true
+                    }
+                }
             }
         });
-        return foundUser;
 
-    }catch(error) {
+        if (!foundUser) {
+            return null;
+        }
+        
+        const { password, ...userWithoutPassword } = foundUser;
+        return userWithoutPassword;
+
+    } catch (error) {
         console.error(error);
         throw new Error("Could not get user with id:", userId);
     }
@@ -20,21 +44,69 @@ const getUserById = async (userId) => {
 
 
 const updateUsername = async (userId, newUsername) => {
-    try{
+    try {
         const updatedUser = prisma.user.update({
             where: {
                 id: userId
             },
             data: {
                 username: newUsername
+            },
+        });
+        return updatedUser;
+
+    } catch (error) {
+        console.error(error);
+        throw new Error("Could not update username");
+    }
+}
+
+
+
+const follow = async (parentId, childId) => {
+    try{
+        const updatedUser = await prisma.user.update({
+            where: {
+                id: parentId
+            },
+            data: {
+                following: {
+                    connect: {
+                        id: childId
+                    }
+                }
             }
         });
         return updatedUser;
 
     }catch(error) {
         console.error(error);
-        throw new Error("Could not update username");
+        throw new Error("Could not follow user with id:", parentId)
     }
 }
 
-export default { getUserById, updateUsername }
+
+
+const unfollow = async (parentId, childId) => {
+    try{
+        const updatedUser = prisma.user.update({
+            where: {
+                id: parentId
+            },
+            data: {
+                following: {
+                    disconnect: {
+                        id: childId
+                    }
+                }
+            }
+        });
+        return updatedUser;
+
+    }catch(error) {
+        console.error(error);
+        throw new Error("Could not unfollow user with id:", parentId);
+    }
+}
+
+export default { getUserById, updateUsername, follow, unfollow }
