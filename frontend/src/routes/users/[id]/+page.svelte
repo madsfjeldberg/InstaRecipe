ba5a232af1939d856daa4fba65e216750f67686d
Chris <chye0001@stud.kea.dk>
2025-05-16 17:29:49 +0200

    begun crafting /users/id page

---
+++ frontend/src/routes/users/[id]/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { onMount } from "svelte";
+    import { page } from "$app/stores";
+
+    import { LoaderCircle } from "lucide-svelte";
+
+    const userId = $page.params.id;
+    let user = $state(null);
+    let loading = $state(true);
+
+    onMount(async () => {
+        user = await getUserById(userId);
+    });
+</script>
+
+{#if loading}
+    <LoaderCircle/> Loading...
+{/if}
+<!-- <h1>Users: {userId} {user.name}</h1> -->
