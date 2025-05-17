8c5e2e9b5b7beb9728230545efc5788f43957bd9
Chris <chye0001@stud.kea.dk>
2025-05-17 11:07:18 +0200

    finished componetns for follow/unfollow

--- frontend/src/lib/components/Follow/FollowButton.svelte
+++ frontend/src/lib/components/Follow/FollowButton.svelte
@@ -1,33 +1,32 @@
 <script>
-    import { onDestroy } from "svelte";
-
+    import { UserRoundPlus } from "lucide-svelte";
     import { Button } from "$lib/components/ui/button/index.js";
     import { toast } from "svelte-sonner";
 
     import { user } from "../../../stores/authStore.js";
     import { socket } from "../../../stores/socketStore.js";
 
-    const { parentUser = $bindable() } = $props();
-
-    const disconnect = socket.on("following", (updatedUser) => {
-        parentUser.followers = [...parentUser.followers, updatedUser.followers]
-    })
-    onDestroy(disconnect)
+    const { parentUser = $bindable(), onToggleFollowButton } = $props();
+    const viewer = $user;
 
     const followUser = () => {
 
-        if(!$user) {
+        if(!viewer) {
             toast.error("You have to login to follow " + parentUser.username)
             return;
         }
 
         const data = {
             parentId: parentUser.id,
-            childId: $user.id
+            childId: viewer.id
         }
 
         socket.emit("following", data);
+        onToggleFollowButton();
     }
 </script>
 
-<Button onclick={followUser}>Follow</Button>
+<Button onclick={followUser}>
+    <UserRoundPlus/>
+    Follow
+</Button>
