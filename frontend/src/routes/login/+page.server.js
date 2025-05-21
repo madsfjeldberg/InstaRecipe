60b4f9aed3a358d9b18a8d06c5afcd3a44e6a663
Chris <chye0001@stud.kea.dk>
2025-05-21 20:55:34 +0200

    enabled returnTo, meaning, when you try to request protected endpoint, you get redirected to login, after login, it will take you to the original requested page

--- frontend/src/routes/login/+page.server.js
+++ frontend/src/routes/login/+page.server.js
@@ -1,8 +1,11 @@
 import { redirect } from '@sveltejs/kit';
 
-export function load({ locals }) {
+export function load({ locals, url }) {
   if (locals.user) {
     // user is already authenticated → short-circuit to /dashboard
     throw redirect(303, '/dashboard');
   }
+  
+  const returnTo = url.searchParams.get('returnTo') || '/dashboard';
+  return { url: url.pathname, returnTo };
 }
