8c8ba24cf4998db98382a059b2f604adba588a03
Chris <chye0001@stud.kea.dk>
2025-06-11 22:42:46 +0200

    removed jwt verification from sveltekit server, this is only handled on own server

--- frontend/src/hooks.server.js
+++ frontend/src/hooks.server.js
@@ -1,16 +1,13 @@
 import { redirect } from '@sveltejs/kit';
 
-import jwt from 'jsonwebtoken';
-
-import { REFRESH_TOKEN_SECERET } from '$lib/config/env.server.js'; 
-
+import authApi from '$lib/api/authApi.js';
 import userApi from '$lib/api/userApi.js';
 
 
 
-const verifyJWT = (token) => {
+const verifyJWT = async (token) => {
   try {
-    const decoded = jwt.verify(token, REFRESH_TOKEN_SECERET);
+    const decoded = await authApi.verifyToken(token);
     return { id: decoded.id, username: decoded.username, email: decoded.email };
 
   } catch (error) {
@@ -22,13 +19,14 @@ export const handle = async ({ event, resolve }) => {
   const token = event.cookies.get('refreshToken');
   if (token) {
     try {
-      const payload = verifyJWT(token);
+      const payload = await verifyJWT(token);
       const user = await userApi.getUserById(payload.id);
       const { avatar, avatarMime, ...modifiedUser } = user;
       event.locals.user = modifiedUser;
 
     } catch (error) {
-      console.error('JWT verification failed:', error);
+      event.cookies.delete('refreshToken', { path: '/' });
+
       event.locals.user = null;
       throw redirect(303, '/login');
     }
