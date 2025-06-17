18093ec7a3c782b1d7222080658e275e819d0f40
Chris Ye <chye0001@kea.stud.dk>
2025-06-17 17:10:28 +0200

    removed unused prisma accelerate

--- backend/database/prismaClient.js
+++ backend/database/prismaClient.js
@@ -1,9 +1,8 @@
 import { PrismaClient } from '../generated/prisma/index.js';
-import { withAccelerate } from '@prisma/extension-accelerate';
 
 const prisma = new PrismaClient({
   log: ["warn", "error"],
   errorFormat: "pretty",
-}).$extends(withAccelerate());
+});
 
 export default prisma;
